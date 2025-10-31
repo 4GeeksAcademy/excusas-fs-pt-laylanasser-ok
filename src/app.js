4cf02ffa872d1e590ee0b4e36468c4e43b1e0f5f
todolayla-git <todolayla@gmail.com>
2025-10-31 12:11:43 +0000

    first Commit

--- src/app.js
+++ src/app.js
@@ -9,19 +9,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
-// datos ejercicio
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-// función 
+//funcion random number
 const randomNumber = arr => {
   return Math.floor(Math.random() * arr.length);
 };
 
-window.onload = function () {
-  const excuse = `${who[randomNumber(who)]} ${action[randomNumber(action)]} ${what[randomNumber(what)]} ${when[randomNumber(when)]}.`;
-  document.getElementById("excuse").innerHTML = excuse;
-  console.log(excuse);
+//funcion para extraer valores arrays
+const getRandomValue = arr => {
+  return arr[randomNumber(arr)];
 };
+
+//funcion concatenar
+const generateExcuse = () => {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  // 4. retornar
+  return `${getRandomValue(who)} ${getRandomValue(action)} ${getRandomValue(what)} ${getRandomValue(when)}.`;
+};
+
+console.log(generateExcuse());
