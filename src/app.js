4775dd012cedf3b3b24aa971810862534d25c487
todolayla-git <todolayla@gmail.com>
2025-10-31 11:46:56 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+// datos ejercicio
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+// función 
+const randomNumber = arr => {
+  return Math.floor(Math.random() * arr.length);
+};
+
+window.onload = function () {
+  const excuse = `${who[randomNumber(who)]} ${action[randomNumber(action)]} ${what[randomNumber(what)]} ${when[randomNumber(when)]}.`;
+  document.getElementById("excuse").innerHTML = excuse;
+  console.log(excuse);
+};
